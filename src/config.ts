279ced17e1e64cf628fd4eb58897f3a671cb5368
Andrew Vos <andrew.vos@gmail.com>
2026-05-09 12:20:14 +0100

    Use Vercel Bitbucket broker

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
   githubClientId: "Ov23lio3oI2E9LiQx6ut",
   bitbucketClientId: "Ksmt2xbH4FabcXHwS7",
-  bitbucketBrokerUrl: "https://chchchchanges.vos.lol",
+  bitbucketBrokerUrl: "https://chchchchanges.vercel.app",
 };
